9e6ccface3f6e3c1eca958894c0accca44b3adda
hikalkan <hi_kalkan@yahoo.com>
2014-10-20 22:44:23 +0300

    Bundling for durandal template.

--- Templates/All-In-One-Template/MySpaProject/MySpaProject.WebSpaDurandal/App_Start/MySpaProjectWebModule.cs
+++ Templates/All-In-One-Template/MySpaProject/MySpaProject.WebSpaDurandal/App_Start/MySpaProjectWebModule.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using Abp.Localization;
 using Abp.Localization.Sources.Xml;
@@ -8,13 +9,17 @@ using Abp.Modules;
 
 namespace MySpaProject.WebSpaDurandal
 {
-    [DependsOn(typeof(MySpaProjectDataModule),typeof(MySpaProjectApplicationModule),typeof(MySpaProjectWebApiModule))]
+    [DependsOn(typeof(MySpaProjectDataModule), typeof(MySpaProjectApplicationModule), typeof(MySpaProjectWebApiModule))]
     public class MySpaProjectWebModule : AbpModule
     {
         public override void PreInitialize()
         {
+            //Add/remove languages for your application
             Configuration.Localization.Languages.Add(new LanguageInfo("en", "English", "famfamfam-flag-england", true));
             Configuration.Localization.Languages.Add(new LanguageInfo("tr", "Türkçe", "famfamfam-flag-tr"));
+
+            //Configure navigation/menu
+            Configuration.Navigation.Providers.Add<MySpaProjectNavigationProvider>();
         }
 
         public override void Initialize()
@@ -23,13 +28,14 @@ namespace MySpaProject.WebSpaDurandal
 
             Configuration.Localization.Sources.Add(
                 new XmlLocalizationSource(
-                    "MySpaProject",
+                    MySpaProjectConsts.LocalizationSourceName,
                     HttpContext.Current.Server.MapPath("~/Localization/MySpaProject")
                     )
                 );
 
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
