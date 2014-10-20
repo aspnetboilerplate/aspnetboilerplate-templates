f57b187ef6b86678402a45bb28be26eb8733801b
hikalkan <hi_kalkan@yahoo.com>
2014-10-20 21:14:16 +0300

    Completed MPA template's language and menu system.

---
+++ Templates/All-In-One-Template/MySpaProject/MySpaProject.WebMpa/Controllers/LayoutController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using Abp.Application.Navigation;
+using Abp.Localization;
+using MySpaProject.WebMpa.Models.Layout;
+
+namespace MySpaProject.WebMpa.Controllers
+{
+    public class LayoutController : MySpaProjectControllerBase
+    {
+        private readonly IUserNavigationManager _userNavigationManager;
+        private readonly ILocalizationManager _localizationManager;
+
+        public LayoutController(IUserNavigationManager userNavigationManager, ILocalizationManager localizationManager)
+        {
+            _userNavigationManager = userNavigationManager;
+            _localizationManager = localizationManager;
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult TopMenu()
+        {
+            var model = new TopMenuViewModel
+                        {
+                            MainMenu = _userNavigationManager.GetMenu("MainMenu", CurrentSession.UserId)
+                        };
+
+            return PartialView("_TopMenu", model);
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult LanguageSelection()
+        {
+            var model = new LanguageSelectionViewModel
+                        {
+                            CurrentLanguage = _localizationManager.CurrentLanguage,
+                            Languages = _localizationManager.GetAllLanguages()
+                        };
+
+            return PartialView("_LanguageSelection", model);
+        }
+    }
+}
---
+++ Templates/All-In-One-Template/MySpaProject/MySpaProject.WebMpa/Models/Layout/TopMenuViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Navigation;
+
+namespace MySpaProject.WebMpa.Models.Layout
+{
+    public class TopMenuViewModel
+    {
+        public UserMenu MainMenu { get; set; }
+    }
+}
