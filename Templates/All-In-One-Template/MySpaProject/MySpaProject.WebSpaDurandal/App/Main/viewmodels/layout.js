55674129514520227112f4612abbdbb3653ae78c
hikalkan <hi_kalkan@yahoo.com>
2014-10-22 20:37:27 +0300

    Menu system changed to ABP's system for durandal.

--- Templates/All-In-One-Template/MySpaProject/MySpaProject.WebSpaDurandal/App/Main/viewmodels/layout.js
+++ Templates/All-In-One-Template/MySpaProject/MySpaProject.WebSpaDurandal/App/Main/viewmodels/layout.js
@@ -9,10 +9,12 @@
             that.languages = abp.localization.languages;
             that.currentLanguage = abp.localization.currentLanguage;
 
+            that.menu = abp.nav.menus.MainMenu;
+
             that.activate = function () {
                 router.map([
-                    { route: '', title: abp.localization.localize('HomePage', 'MySpaProject'), moduleId: 'viewmodels/home', nav: true },
-                    { route: 'about', title: abp.localization.localize('About', 'MySpaProject'), moduleId: 'viewmodels/about', nav: true }
+                    { route: '', title: abp.localization.localize('HomePage', 'MySpaProject'), moduleId: 'viewmodels/home', nav: true, menuName: 'Home' },
+                    { route: 'about', title: abp.localization.localize('About', 'MySpaProject'), moduleId: 'viewmodels/about', nav: true, menuName: 'About' }
                 ]).buildNavigationModel();
 
                 return that.router.activate();
