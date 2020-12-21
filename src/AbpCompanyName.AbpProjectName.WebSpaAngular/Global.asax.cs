8e1b8d0fa3690af8cc21e933b0e6c4363cec759d
Musa Demir <musa.demir@volosoft.com>
2020-12-21 15:28:51 +0300

    implement production config for log4net

--- src/AbpCompanyName.AbpProjectName.WebSpaAngular/Global.asax.cs
+++ src/AbpCompanyName.AbpProjectName.WebSpaAngular/Global.asax.cs
@@ -9,9 +9,15 @@ namespace AbpCompanyName.AbpProjectName.WebSpaAngular
     {
         protected override void Application_Start(object sender, EventArgs e)
         {
+#if DEBUG
             AbpBootstrapper.IocManager.IocContainer.AddFacility<LoggingFacility>(
-                            f => f.UseAbpLog4Net().WithConfig(Server.MapPath("log4net.config"))
-                        );
+                f => f.UseAbpLog4Net().WithConfig(Server.MapPath("log4net.config"))
+            );
+#else
+            AbpBootstrapper.IocManager.IocContainer.AddFacility<LoggingFacility>(
+                f => f.UseAbpLog4Net().WithConfig(Server.MapPath("log4net.Production.config"))
+            );
+#endif
 
             base.Application_Start(sender, e);
         }
