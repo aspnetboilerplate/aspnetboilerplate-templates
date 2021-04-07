2b7d35191af6c14eecd902d5b725770f3ffc45e7
Musa Demir <demirmusa96@gmail.com>
2021-04-07 16:56:38 +0300

    change DefaultPassPhrase for every project
    The {{DEFAULT_PASS_PHRASE_HERE}} placeholder will be replaced by website when the project is downloaded.

--- src/AbpCompanyName.AbpProjectName.Core/AbpProjectNameCoreModule.cs
+++ src/AbpCompanyName.AbpProjectName.Core/AbpProjectNameCoreModule.cs
@@ -5,6 +5,11 @@ namespace AbpCompanyName.AbpProjectName
 {
     public class AbpProjectNameCoreModule : AbpModule
     {
+        public override void PreInitialize()
+        {
+            Configuration.Settings.SettingEncryptionConfiguration.DefaultPassPhrase = AbpProjectNameConsts.DefaultPassPhrase;
+        }
+
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
